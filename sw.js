f161a68360c277347e532dec45654a299d8c11f5
sales403 <sales@veneradolls.com>
2026-09-06 23:25:40 +0200

    Bust app cache for tariff v10.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sos-rider-v10-unified-20260906-syncfix-1021';
+const CACHE='sos-rider-v10-20260906-tariffe-1030';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-sos-rider.png','./rider-banner-v2-live.png','./icons.svg'];
 
 self.addEventListener('install',e=>{
