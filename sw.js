592fcd91f28aee4496d5020d079ce973e1c5a0de
sales403 <sales@veneradolls.com>
2026-09-07 11:42:13 +0200

    Refresh app cache for WhatsApp parser update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sos-rider-v10-20260907-hotfix-1031';
+const CACHE='sos-rider-v10-20260907-whatsapp-1032';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-sos-rider.png','./rider-banner-v2-live.png','./icons.svg'];
 
 self.addEventListener('install',e=>{
